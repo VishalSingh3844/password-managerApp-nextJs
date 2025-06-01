144942ac1c004cf69bf0609f2af02861919fd4e7
acharnabin <acharnabin@gmail.com>
2025-06-01 10:38:21 +0530

    Shacn set up , sign up validation , login form ui design

---
+++ src/feature/authentication/components/sign-up-form.tsx
@@ -0,0 +1,109 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import * as z from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const signUpSchema = z.object({
+  name: z.string().trim().min(1),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type TSignUpSchema = z.infer<typeof signUpSchema>;
+
+export default function SignUpForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TSignUpSchema>({
+    resolver: zodResolver(signUpSchema),
+  });
+
+  const onSubmit:SubmitHandler<TSignUpSchema> = (data) => {
+    console.log(data);
+  };
+
+  console.log(errors)
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign up to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to sign up to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Name</Label>
+                <Input
+                  id="name"
+                  
+                  placeholder="Enter name"
+                  hasError={Boolean(errors?.name)}
+                  error={errors?.name?.message}
+                  {...register("name")}
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  {...register("email")}
+                  hasError={Boolean(errors?.email)}
+                  error={errors?.email?.message}
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  {...register("password")}
+                  hasError={Boolean(errors?.password)}
+                  error={errors?.password?.message}
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Sign up
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Sign up with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Login
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
